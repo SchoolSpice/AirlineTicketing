d68770ede99ed9dedb288a3d856bd70a96d52fb1
SchoolSpice <lyana.curry.358@my.csun.edu>
2021-12-06 15:23:13 -0800

    Hyphen Acceptance

--- Comp380/src/AirlineTicketing/KioskTerminal.java
+++ Comp380/src/AirlineTicketing/KioskTerminal.java
@@ -318,7 +318,7 @@ public class KioskTerminal {
 	} // end-enterInfo
 
 	private static boolean isOnlyLetters(String s) {
-		return s.matches("[ a-zA-Z]+[a-z-']*$");
+		return s.matches("[ -a-zA-Z]+[a-z-']*$");
 	} // end-isOnlyLetters
 
 	private static boolean isValidEmail(String s) {
@@ -427,7 +427,7 @@ public class KioskTerminal {
 			System.out.println("Unable to initialize database.");
 			return;
 		}
-		data.runSQL(query);
+		//data.runSQL(query);
 		input.close();
 	} // end-enterSQL
 
